88d1232eb1a6a731dc0c4bfc3a6ef1b8cd72ad48
Micheal987 <noah1145@outlook.com>
2026-07-22 01:30:21 +0800

    struct

--- day1/core.c
+++ day1/core.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
 #include "config.h"
 
-int config()
-{
-    return init(20);
-}
+int config() { return init(20); }
--- struct/main.c
+++ struct/main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 struct ApiData
 {
     int code;
@@ -13,24 +14,31 @@ struct User
     char name[50];
 };
 
+typedef struct
+{
+    int code;
+    char name[50];
+} Sys;
 int main()
 {
     // 初始化一筆資料
     struct User my_user = {
-        .api = {.code = 200, .msg = "Success"},
-        .id = 1001,
-        .name = "linux"};
+        .api = {.code = 200, .msg = "Success"}, .id = 1001, .name = "linux"};
 
     // 模擬：我們現在只拿到通用外殼的指標（例如在底層網路傳輸、框架回傳時）
     struct ApiData *api_ptr = &my_user.api;
 
     // 神奇魔法：利用著名的 container_of 巨集技術，
     // 透過計算記憶體偏移量（Offset），直接從外殼「反向回推」拿到整份 User 資料！
-    struct User *user_ptr = (struct User *)((char *)api_ptr - offsetof(struct User, api));
+    struct User *user_ptr =
+        (struct User *)((char *)api_ptr - offsetof(struct User, api));
 
     // 成功取出資料！
     printf("Code: %d, Msg: %s\n", api_ptr->code, api_ptr->msg);
     printf("User ID: %d, Name: %s\n", user_ptr->id, user_ptr->name);
-
+    Sys sysInfo;
+    sysInfo.code = 10;
+    strcpy(sysInfo.name, "Linux");
+    printf("Code: %d, Name: %s\n", sysInfo.code, sysInfo.name);
     return 0;
 }
